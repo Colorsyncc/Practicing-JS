631be17e427321448d1d745f8d1ae91984ddec36
ColorSync <y.pezoa22@gmail.com>
2020-03-29 17:43:40 -0300

    added function to expired recaptcha

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,31 +1,30 @@
 import React, { Component } from 'react';
-import { ReCaptcha } from 'react-recaptcha-google';
+import Recaptcha from 'react-google-recaptcha';
 import '../components/input.css';
 
 class Input extends Component {
 
     componentDidMount() {
-        if (this.captchaDemo) {
-            console.log("started, just a second...")
-            this.captchaDemo.reset();
-        }
+        this.captchaDemo ? this.captchaDemo.reset() : this.captchaDemo = false;
     }
 
-    onLoadReCaptcha() {
-        if (this.captchaDemo) {
-            this.captchaDemo.reset();
-        }
-    }
-
-    verifyCallback(recaptchaToken) {
+    verifyCallback = recaptchaToken => {
         // Here you will get the final recaptchaToken!!!  
-        const buttonDisable = document.querySelector('.buttonEnable')
+        const buttonDisable = document.querySelector('.buttonEnable');
         recaptchaToken ? buttonDisable.disabled = false : buttonDisable.disabled = true;
+        console.log(recaptchaToken,'<--- token');
     }
 
-    validateInput(event) {
+    validateInput = event => {
         const fieldMessage = document.querySelector('.fieldError');        
         /[^a-zA-Z ]+/g.test(event.value) ? fieldMessage.hidden = false : fieldMessage.hidden = true;
+    }
+
+    onResetReCaptcha = () => {
+        const buttonDisable = document.querySelector('.buttonEnable');
+        buttonDisable.disabled = true;
+        console.log("Captcha expirado");
+        return this.recaptcha.reset();
     }
 
     render() {
@@ -39,11 +38,12 @@ class Input extends Component {
                     <small className="fieldError" hidden>Introduzca solo letras (A-Z)</small>
                 </div>
                 <button type="submit" className="btn btn-dark buttonEnable" disabled>Aceptar</button>
-                <ReCaptcha
+                <Recaptcha
+                    ref={e => this.recaptcha = e}
                     render="explicit"
                     sitekey="6LcOZOQUAAAAABM1BWGqno2TX-8lGvMlBSI_mnR8"
-                    onloadCallback={this.onLoadRecaptcha}
-                    verifyCallback={this.verifyCallback}
+                    onChange={this.verifyCallback}
+                    onExpired={this.onResetReCaptcha}
                 />
             </div>
     )}
